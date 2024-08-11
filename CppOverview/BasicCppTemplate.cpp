087f38377eb970c59c6b01348fc5d093c36735b1
Gauravgarje <garjegaurav2000@gmail.com>
2024-08-11 11:28:14 +0530

    Added basic template file

---
+++ CppOverview/BasicCppTemplate.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h> //include entire standard library
+
+using namespace std; // declares that classes and functions of the standard library can be used directly in the code
+
+int main() {
+    cout<<"Hello World";
+}
