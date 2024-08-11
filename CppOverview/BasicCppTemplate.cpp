820e49bc942f4da99ad02c9deded87b38c904a9b
Gauravgarje <garjegaurav2000@gmail.com>
2024-08-11 12:01:44 +0530

    Additional content in README file

--- CppOverview/BasicCppTemplate.cpp
+++ CppOverview/BasicCppTemplate.cpp
@@ -2,6 +2,17 @@
 
 using namespace std; // declares that classes and functions of the standard library can be used directly in the code
 
-int main() {
-    cout<<"Hello World";
+int main()
+{
+
+    string name;
+    cout << "Enter your name: ";
+
+    // following two lines of code is required to make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> name;
+    cout << "Hello " << name << "\n";
+    cout << "How are you?";
 }
---
+++ CppOverview/ScanfPrintfDemo.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: \n");
+    scanf("%d %d", &a, &b);
+    printf("You have entered %d and %d\n", a, b);
+}
